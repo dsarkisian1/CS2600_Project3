d8bf327be91dfd089d7b5fac6d305856ec1fff0b
Christian Badoy <clbadoy@cpp.edu>
2021-10-25 01:11:06 -0700

    Added save_file functionality.

--- address_book_fops.c
+++ address_book_fops.c
@@ -77,15 +77,15 @@ Status save_file(AddressBook *address_book)
 	fprintf(address_book->fp, "%i\n", address_book->count);
 	
 	for(int ind = 0; ind < address_book->count; ind++)
-		{
-			fprintf(address_book->fp, "%i,", ind);
-			fprintf(address_book->fp, "%s,", address_book->list[ind].name[0]);
-			for(int j = 0; j < 5; j++)
-				fprintf(address_book->fp, "%s,", address_book->list[ind].phone_numbers[j]);
-			for(int k = 0; k < 5; k++)
-				fprintf(address_book->fp, "%s,", address_book->list[ind].email_addresses[k]);
-			fprintf(address_book->fp, "\n");
-		}
+	{
+		fprintf(address_book->fp, "%i,", ind);
+		fprintf(address_book->fp, "%s,", address_book->list[ind].name[0]);
+		for(int j = 0; j < 5; j++)
+			fprintf(address_book->fp, "%s,", address_book->list[ind].phone_numbers[j]);
+		for(int k = 0; k < 5; k++)
+			fprintf(address_book->fp, "%s,", address_book->list[ind].email_addresses[k]);
+		fprintf(address_book->fp, "\n");
+	}
 
 	fclose(address_book->fp);
 
